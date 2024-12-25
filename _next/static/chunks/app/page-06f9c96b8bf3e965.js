583627999b7b24f24105630a33eaab56b2af7c58
ajay.panthagani <ajay.panthagani@ninjavan.co>
2024-12-26 05:18:23 +0530

    Updates

---
+++ _next/static/chunks/app/page-06f9c96b8bf3e965.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{1101:(e,s,_)=>{Promise.resolve().then(_.bind(_,3604))}},e=>{var s=s=>e(e.s=s);e.O(0,[711,380,287,565,258,441,517,358],()=>s(1101)),_N_E=e.O()}]);
