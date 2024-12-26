dd1b8e08fa7b63439ab48f20f7c8ae55b874d484
ajay.panthagani <ajay.panthagani@ninjavan.co>
2024-12-26 05:36:29 +0530

    deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/sizzlie/' : '',
-  basePath: isProd ? '/sizzlie' : '',
+  assetPrefix: '/sizzlie/',
+  basePath: '/sizzlie',
   output: 'export'
 };
 
