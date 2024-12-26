32f49df1fcd69c5935480b42c3610a057857cbdb
ajay.panthagani <ajay.panthagani@ninjavan.co>
2024-12-26 05:42:34 +0530

    deployment config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: '/sizzlie/',
+  assetPrefix: '/sizzlie',
   basePath: '/sizzlie',
   output: 'export'
 };
